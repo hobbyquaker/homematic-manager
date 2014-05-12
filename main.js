187de97fd91a16df018631029e3726c0d3271460
hobbyquaker <hobbyquaker@gmail.com>
2014-05-12 18:27:28 +0200

    load rega objects

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ var fs = require("fs");
 var express = require("express");
 var socketio = require("socket.io");
 var xmlrpc = require("xmlrpc");
+var http = require("http");
 
 var config = loadConfig();
 var logStream = openLog(__dirname + "/log/hm-manager.log");
@@ -20,11 +21,18 @@ var regaCache = {};
 
 initWebServer();
 initSocket();
-getRegaObjects();
 
-function getRegaObjects() {
 
+for (var daemon in config.daemons) {
+    if (config.daemons[daemon].isCcu && !regaCache[config.daemons[daemon].ip]) {
+        regaCache[config.daemons[daemon].ip] = {};
+        getRegaObjects(config.daemons[daemon].ip);
+    }
 }
+
+
+
+
 
 function initSocket() {
 
@@ -32,6 +40,10 @@ function initSocket() {
 
         socket.on("getConfig", function (callback) {
             callback(config);
+        });
+
+        socket.on("getRegaObjects", function (callback) {
+            callback(regaCache);
         });
 
         socket.on("bidcosConnect", function (daemon, callback) {
@@ -94,6 +106,72 @@ function initWebServer() {
 
 
 }
+
+
+function getRegaObjects(ip) {
+    console.log("loading rega devices from " + ip);
+    regaScript(ip, "devices.fn", function (res) {
+        regaCache[ip].devices = res;
+        console.log("loading rega channels from " + ip);
+        regaScript(ip, "channels.fn", function (res) {
+            regaCache[ip].channels = res;
+            console.log("loading rega rooms from " + ip);
+            regaScript(ip, "rooms.fn", function (res) {
+                regaCache[ip].rooms = res;
+                console.log("loading rega functions from " + ip);
+                regaScript(ip, "functions.fn", function (res) {
+                    regaCache[ip].functions = res;
+                });
+            });
+        });
+    });
+}
+
+function regaScript(ip, file, callback) {
+    fs.readFile(__dirname + '/regascripts/' + file, 'utf8', function (err, script) {
+        if (err) {
+            console.log("regaScript " + err);
+            return false;
+        }
+        var post_options = {
+            host: ip,
+            port: '8181',
+            path: '/rega.exe',
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
+                'Content-Length': script.length
+            }
+        };
+        var post_req = http.request(post_options, function(res) {
+            var data = "";
+            res.setEncoding('utf8');
+            res.on('data', function (chunk) {
+                data += chunk.toString();
+            });
+            res.on('end', function () {
+                var pos = data.lastIndexOf("<xml>");
+                var stdout = (data.substring(0, pos));
+                try {
+                    var result = JSON.parse(stdout);
+                    callback(result);
+                } catch (e) {
+                    console.log("regaScript error " + e);
+                }
+            });
+        });
+
+        post_req.on('error', function (e) {
+            console.log("regaScript POST " + e);
+        });
+
+        post_req.write(script);
+        post_req.end();
+
+    });
+}
+
+
 
 function loadConfig() {
     return JSON.parse(fs.readFileSync(__dirname + "/config.json"));

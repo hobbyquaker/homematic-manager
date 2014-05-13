ec24c4d443778ce839f88569005b34f5aa41675f
hobbyquaker <hobbyquaker@gmail.com>
2014-05-13 22:06:50 +0200

    text for param type enum

--- www/js/homematic-manager.js
+++ www/js/homematic-manager.js
@@ -158,7 +158,7 @@ $(document).ready(function () {
                 case "ENUM":
                     input = '<select id="paramset-select-' + param + '"' + (desc[param].OPERATIONS & 2 ? '' : ' disabled="disabled"') + '>';
                     for (var i = desc[param].MIN; i <= desc[param].MAX; i++) {
-                        input += '<option value="' + i + '">' + i + '</option>';
+                        input += '<option value="' + i + '">' + desc[param].VALUE_LIST[i] + '</option>';
                     }
                     input += '</select>';
                     break;
